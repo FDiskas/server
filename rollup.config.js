c30047143da6c57cfb79a4a34e0c2cddbdb26f37
Vytenis Kuciauskas <projektas@gmail.com>
2020-12-17 16:31:54 +0200

    Replace bundler to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,49 @@
-import multi from '@rollup/plugin-multi-entry';
-// import { resolve } from 'path';
+// @ts-check
+import multi from 'rollup-plugin-multi-input';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import readConfig from 'properties-reader';
+import { readFileSync } from 'fs';
+import resolve from 'rollup-plugin-node-resolve';
+import copy from 'rollup-plugin-copy-glob';
 
-const serverConfigFile = './server.cfg';
-const serverConfig = readConfig(serverConfig);
-const serverConfigResources = serverConfig.get('resources').map((item) => `src/${item}/server/server.ts`);
-const clientConfigResources = serverConfig.get('resources').map((item) => `src/${item}/client/client.ts`);
+function looseJsonParse(obj) {
+    return Function('"use strict";return (' + obj + ')')();
+}
+
+const serverConfigFile = 'server.cfg';
+const serverConfigContent = readFileSync(serverConfigFile, 'utf8').replace(/\#/gi, '//');
+const serverConfig = looseJsonParse(`{ ${serverConfigContent} }`);
+
+const serverConfigResources = serverConfig.resources.map((item) => `src/${item}/server/server.ts`);
+const clientConfigResources = serverConfig.resources.map((item) => `src/${item}/client/client.ts`);
+const clientHtmlResources = serverConfig.resources.map((item) => ({
+    files: `src/${item}/client/html/**/*`,
+    dest: `resources/${item}/client/html`,
+}));
+const resourceSettings = serverConfig.resources.map((item) => ({
+    files: `src/${item}/resource.cfg`,
+    dest: `resources/${item}/`,
+}));
+const resourceFromSrcCopy = serverConfig.resources.map((item) => ({
+    files: `src-copy/${item}/**/*`,
+    dest: `resources/${item}/`,
+}));
 
 export default {
     input: [...serverConfigResources, ...clientConfigResources],
     output: {
         dir: 'resources',
     },
+    treeshake: true,
     external: ['alt-client', 'alt-server', 'natives'],
-    plugins: [multi(), typescript({ abortOnError: true }), terser()],
+    plugins: [
+        multi(),
+        resolve(),
+        typescript({ abortOnError: true }),
+        terser(),
+        copy([...clientHtmlResources, ...resourceSettings, ...resourceFromSrcCopy], {
+            verbose: true,
+            watch: false,
+        }),
+    ],
 };
