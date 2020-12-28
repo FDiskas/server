ed7a0db7335abb23d8b12a3c67c5642ebbb2e91f
Vytenis Kuciauskas <projektas@gmail.com>
2020-12-28 16:24:58 +0200

    Object picup concept from https://github.com/LeonMrBonnie/altv-os-pickups

---
+++ src/object-pickup/client/client.ts
@@ -0,0 +1,56 @@
+import * as alt from 'alt-client';
+import * as native from 'natives';
+
+let pickups = {};
+let streamer = new alt.WebView('http://resource/client/html/streamer.html');
+
+alt.on('keydown', (key) => {
+    if (key === 'C'.charCodeAt(0)) {
+        alt.log('~g~ C is pressed');
+        alt.emitServer('create', alt.Player.local.pos);
+    }
+});
+alt.setInterval(() => {
+    let pos = alt.Player.local.pos;
+    streamer.emit('updatePlayerPos', pos.x, pos.y, pos.z);
+}, 2000);
+
+streamer.on('createObject', (name, model, pos) => {
+    pickups[name] = native.createObject(model, pos.x, pos.y, pos.z, false, false, false);
+    native.freezeEntityPosition(pickups[name], true);
+    native.setEntityCollision(pickups[name], false, false);
+});
+
+streamer.on('removeObject', (name) => {
+    native.deleteObject(pickups[name]);
+    delete pickups[name];
+});
+
+alt.onServer('pickups:create', (name, model, pos) => {
+    streamer.emit('addObject', name, alt.hash(model), pos.x, pos.y, pos.z);
+});
+
+alt.onServer('pickups:remove', (name) => {
+    let pickup = pickups[name];
+    if (pickup) {
+        native.deleteObject(pickup);
+        delete pickups[name];
+    }
+    streamer.emit('removeObject', name);
+});
+
+native.setAudioFlag('LoadMPData', true);
+alt.onServer('pickups:pickup', (sound, soundSet) => {
+    native.playSoundFrontend(-1, sound, soundSet, true);
+});
+
+alt.everyTick(() => {
+    let frametime = native.timestep();
+    for (let name in pickups) {
+        let obj = pickups[name];
+        let rot = native.getEntityRotation(obj, 2);
+        let pos = native.getEntityCoords(obj, true);
+        native.setEntityRotation(obj, rot.x, rot.y, rot.z + 90 * frametime, 2, true);
+        native.drawLightWithRangeAndShadow(pos.x, pos.y, pos.z, 255, 255, 255, 2.5, 3.5, 15.0);
+    }
+});
---
+++ src/object-pickup/server/server.ts
@@ -0,0 +1,143 @@
+import * as alt from 'alt-server';
+
+class Pickups {
+    static _pickups = {};
+    _name: any;
+    _model: any;
+    _position: any;
+    _dimension: any;
+    _respawn: any;
+    _respawnTime: any;
+    _disabled: boolean;
+    _pickupSound: { name: any; set: any };
+    _pickupColshape: alt.ColshapeCylinder;
+    _respawnTimeout: number;
+
+    static create(
+        name,
+        model,
+        position,
+        dimension = 0,
+        respawn = false,
+        respawnTime = 30000,
+        pickupSound = 'Deliver_Pick_Up',
+        pickupSoundSet = 'HUD_FRONTEND_MP_COLLECTABLE_SOUNDS'
+    ) {
+        if (Pickups._pickups[name]) return;
+        alt.log(
+            JSON.stringify({
+                pickups: Pickups._pickups,
+                name,
+                model,
+                position,
+                dimension,
+                respawn,
+                respawnTime,
+                pickupSound,
+                pickupSoundSet,
+            })
+        );
+        new Pickups(name, model, position, dimension, respawn, respawnTime, pickupSound, pickupSoundSet);
+    }
+    static remove(name) {
+        let pickup = Pickups._pickups[name];
+        if (!pickup) return;
+        pickup.removeColshapes();
+        if (pickup._respawnTimeout) alt.clearTimeout(pickup._respawnTimeout);
+
+        delete Pickups._pickups[name];
+        alt.emitClient(null, 'pickups:remove', name);
+    }
+    static handleEnterColshape(colshape, entity) {
+        if (!entity instanceof alt.Player) return;
+
+        if (colshape.isPickupColshape) colshape.ownerPickup.onPickup(entity);
+    }
+    static handlePlayerConnect(player) {
+        for (const name in Pickups._pickups) {
+            Pickups._pickups[name].createForPlayer(player);
+        }
+    }
+
+    constructor(name, model, position, dimension, respawn, respawnTime, pickupSound, pickupSoundSet) {
+        this._name = name;
+        this._model = model;
+        this._position = position;
+        this._dimension = dimension;
+        this._respawn = respawn;
+        this._respawnTime = respawnTime;
+        this._disabled = false;
+        this._pickupSound = {
+            name: pickupSound,
+            set: pickupSoundSet,
+        };
+
+        Pickups._pickups[name] = this;
+        this.createColshapes();
+        this.createForPlayer(null);
+    }
+    createColshapes() {
+        this._pickupColshape = new alt.ColshapeCylinder(this.position.x, this.position.y, this.position.z, 1.5, 1.5);
+        this._pickupColshape.isPickupColshape = true;
+        this._pickupColshape.ownerPickup = this;
+    }
+    removeColshapes() {
+        this._pickupColshape.destroy();
+    }
+    onPickup(player) {
+        if (player.dimension !== this.dimension) return;
+        if (this._disabled) return;
+        this._disabled = true;
+        this.removeForPlayer(null);
+        alt.emitClient(player, 'pickups:pickup', this.pickupSound.name, this.pickupSound.set);
+        alt.emit('pickups:pickedUp', player, this.name);
+        if (this.respawn)
+            this._respawnTimeout = alt.setTimeout(() => {
+                this._disabled = false;
+                this.createForPlayer(null);
+            }, this.respawnTime);
+        else Pickups.remove(this.name);
+    }
+    createForPlayer(player) {
+        alt.emitClient(player, 'pickups:create', this.name, this.model, this.position);
+    }
+    removeForPlayer(player) {
+        alt.emitClient(player, 'pickups:remove', this.name);
+    }
+
+    get name() {
+        return this._name;
+    }
+    get model() {
+        return this._model;
+    }
+    get position() {
+        return this._position;
+    }
+    get dimension() {
+        return this._dimension;
+    }
+    get respawn() {
+        return this._respawn;
+    }
+    get respawnTime() {
+        return this._respawnTime;
+    }
+    get pickupSound() {
+        return this._pickupSound;
+    }
+}
+
+alt.on('entityEnterColshape', Pickups.handleEnterColshape);
+alt.on('playerConnect', Pickups.handlePlayerConnect);
+
+alt.on('pickups:create', Pickups.create);
+alt.on('pickups:remove', Pickups.remove);
+alt.on('pickups:setStreamRange', (range: string) => {
+    STREAM_RANGE = range;
+});
+
+alt.onClient('create', (player, coords) => {
+    alt.log('~g~ From client');
+    alt.emit('pickups:create', 'trash', 'prop_rub_binbag_05', coords);
+});
