202c415fdc4dafc9e9ba585b014f08ce8022e73a
Vytenis Kuciauskas <projektas@gmail.com>
2021-02-14 15:50:46 +0200

    Improve open close windows by pressing E

--- src/core/client/vehicle/doors.ts
+++ src/core/client/vehicle/doors.ts
@@ -1,51 +1,12 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 import { Action } from '../enums/actions';
+import { soundList } from '../enums/sounds';
 import { distance, getClosestVehicle } from '../lib/distance';
-
-const windowList = [
-    'window_lf',
-    'window_rf',
-    'window_lr',
-    'window_rr',
-    'window_lm',
-    'window_rm',
-    'window_lf1',
-    'window_lf2',
-    'window_lf3',
-    'window_rf1',
-    'window_rf2',
-    'window_rf3',
-    'window_lr1',
-    'window_lr2',
-    'window_lr3',
-    'window_rr1',
-    'window_rr2',
-    'window_rr3',
-];
 
 alt.on(Action.PlayerToggleCarWindow, () => {
     if (alt.Player.local.vehicle) {
-        let availableWindows = windowList.filter(
-            (windowName) => native.getEntityBoneIndexByName(alt.Player.local.vehicle.scriptID, windowName) !== -1
-        );
-
-        const data = { window: -1, distance: 0 };
-        for (const windowIndex in availableWindows) {
-            const windowBone = native.getEntityBoneIndexByName(
-                alt.Player.local.vehicle.scriptID,
-                availableWindows[windowIndex]
-            );
-            const windowPos = native.getWorldPositionOfEntityBone(alt.Player.local.vehicle.scriptID, windowBone);
-            const windowDist = distance(alt.Player.local.pos, { ...windowPos });
-
-            if (windowDist < data.distance || data.distance == 0) {
-                data.window = parseInt(windowIndex);
-                data.distance = windowDist;
-            }
-        }
-
-        alt.emitServer(Action.PlayerToggleCarWindow, data.window);
+        alt.emitServer(Action.PlayerToggleCarWindow, alt.Player.local.seat - 1);
     }
 });
 
@@ -115,9 +76,11 @@ alt.on(Action.PlayerToggleCarDoor, () => {
 // TODO: remove workaround https://github.com/altmp/altv-issues/issues/737
 alt.onServer(Action.PlayerOpenCarWindow, (windowIndex) => {
     native.rollDownWindow(alt.Player.local.vehicle.scriptID, windowIndex);
+    alt.emitServer(Action.PlayerPlaySound, soundList.openWindow, alt.Player.local.scriptID);
 });
 alt.onServer(Action.PlayerCloseCarWindow, (windowIndex) => {
     native.rollUpWindow(alt.Player.local.vehicle.scriptID, windowIndex);
+    alt.emitServer(Action.PlayerPlaySound, soundList.openWindow, alt.Player.local.scriptID);
 });
 alt.onServer(Action.PlayerOpenCarDoor, (vehicle: alt.Vehicle, doorIndex) => {
     if (doorIndex >= 4) {
